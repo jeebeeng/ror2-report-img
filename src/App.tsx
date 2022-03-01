0729fd04c455a424f40cd6eb62fc43693643391b
John Bang <jeebeeng@gmail.com>
2022-02-28 22:40:25 -0500

    Add file input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import { XMLParser, XMLValidator } from 'fast-xml-parser'
+import fs from 'fs'
 
 function App() {
-  return <div></div>
+  const [contents, setContents] = useState()
+
+  const showFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = e => {
+      const text = e.target!.result
+      console.log(text)
+    }
+    reader.readAsText(e.target.files![0])
+  }
+  return (
+    <div>
+      <input type="file" onChange={showFile} />
+    </div>
+  )
 }
 
 export default App
