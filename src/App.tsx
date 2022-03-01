2084701c6f2015656800975d4521ea484035d368
John Bang <jeebeeng@gmail.com>
2022-02-28 23:19:58 -0500

    Define RunReport interface and parse xml

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,64 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import { XMLParser, XMLValidator } from 'fast-xml-parser'
-import fs from 'fs'
+import { XMLParser } from 'fast-xml-parser'
+
+interface RunReport {
+  version: number
+  difficuly: string
+  gameEnding: string
+  gameMode: string
+  runTime: number
+  playerInfos: PlayerInfo[]
+  artifacts: string[]
+}
+
+interface PlayerInfo {
+  name: string
+  bodyName: string
+  equipment: string
+  items: Item[]
+  finalMessageToken: string
+  killerBodyName: string
+  statSheet: StatSheet
+}
+
+interface Item {
+  name: string
+  count: number
+}
+
+interface StatSheet {
+  highestDamageDealt: number
+  highestLevel: number
+  highestStagesCompleted: number
+  totalDamageDealt: number
+  totalDamageTaken: number
+  totalDistanceTravelled: number
+  totalDronesPurchased: number
+  totalEliteKills: number
+  totalGoldCollected: number
+  totalGoldPurchases: number
+  totalHealthHealed: number
+  totalItemsCollected: number
+  totalKills: number
+  totalLunarPurchases: number
+  totalMinionDamageDealth: number
+  totalMinionKills: number
+  totalPurchases: number
+  totalTurretPurchased: number
+}
 
 function App() {
-  const [contents, setContents] = useState()
+  const [report, setReport] = useState<RunReport | null>(null)
 
   const showFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
     const reader = new FileReader()
     reader.onload = e => {
-      const text = e.target!.result
-      console.log(text)
+      const parser = new XMLParser()
+      const xmlStr = e.target!.result
+      let jObj = parser.parse(xmlStr as string)
+      console.log(jObj)
     }
     reader.readAsText(e.target.files![0])
   }
