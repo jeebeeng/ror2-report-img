65281883b90c104e4fb12b0bff783f296f4e9ed5
John Bang <jeebeeng@gmail.com>
2022-03-02 21:22:53 -0500

    Make parameters type of respective enum

--- src/utils/nameUtils.ts
+++ src/utils/nameUtils.ts
@@ -1,27 +1,29 @@
-export const artifactImg = (name: string): string => {
+import { Artifact, BodyName, Difficulty, EclipseLevel, Equipment, Item, Survivor } from "../types";
+
+export const artifactImg = (name: Artifact): string => {
   return '../images/artifacts/Artifact_of_' + name + '.png'
 }
 
-export const bodyNameImg = (name: string): string => {
+export const bodyNameImg = (name: BodyName): string => {
   return '../images/bodies/' + name.replace(' ', '_') + '.png'
 }
 
-export const difficultyImg = (name: string): string => {
+export const difficultyImg = (name: Difficulty): string => {
   return '../images/difficulty/' + name + '.png'
 }
 
-export const eclipseImg = (level: string): string => {
+export const eclipseImg = (level: EclipseLevel): string => {
   return '../images/eclipse/Eclipse' + level + '.png'
 }
 
-export const equipmentImg = (name: string): string => {
+export const equipmentImg = (name: Equipment): string => {
   return '../images/equipment/' + name.replace(' ', '_') + '.png'
 }
 
-export const itemImg = (name: string): string => {
+export const itemImg = (name: Item): string => {
   return '../images/items/' + name.replace(' ', '_') + '.png'
 }
 
-export const survivorImg = (name: string): string => {
+export const survivorImg = (name: Survivor): string => {
   return '../images/survivors/' + name.replace(' ', '_') + '.png'
 }
