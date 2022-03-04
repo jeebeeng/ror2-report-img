534cf89a77caea7e503917dcdfb0dc4c65b1f075
John Bang <jeebeeng@gmail.com>
2022-03-03 19:08:53 -0500

    Add ItemIcon component

---
+++ src/components/ItemIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ItemData } from '../types'
+import * as path from '../utils/nameUtils'
+
+interface ItemIconProps {
+  item: ItemData
+}
+
+const ItemIcon: React.FC<ItemIconProps> = ({ item }) => {
+  return (
+    <div className="w-12 relative">
+      <img src={path.itemImg(item.name)} alt={item.name} />
+      <p className="absolute top-0 right-0">
+        {item.count > 1 ? `x${item.count}` : null}
+      </p>
+    </div>
+  )
+}
+
+export default ItemIcon
