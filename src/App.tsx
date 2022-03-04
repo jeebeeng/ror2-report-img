93badaf8714deb70e343a1ed27b36c846438c860
John Bang <jeebeeng@gmail.com>
2022-03-04 14:46:36 -0500

    Add invalid report error message

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import FileInput from './components/FileInput'
 
 function App() {
   const [report, setReport] = useState<RunReport | null>(null)
+  const [error, setError] = useState<boolean>(false)
 
   return (
     <div className="bg-white min-h-screen flex flex-col items-center">
       <h1 className="text-6xl font-rubik tracking-wide my-4">RISK REPORT</h1>
-      <FileInput setReport={setReport} />
-      {report != null ? <Report report={report!} /> : null}
+      <FileInput setReport={setReport} setError={setError} />
+      {error && <h2 className="text-red-600 font-bold">Invalid Report</h2>}
+      {report !== null && !error ? <Report report={report!} /> : null}
     </div>
   )
 }
--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -5,9 +5,10 @@ import { XMLParser } from 'fast-xml-parser'
 
 interface FileInputProps {
   setReport: React.Dispatch<React.SetStateAction<RunReport | null>>
+  setError: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const FileInput: React.FC<FileInputProps> = ({ setReport }) => {
+const FileInput: React.FC<FileInputProps> = ({ setReport, setError }) => {
   const fileInput = useRef<HTMLInputElement>(null)
 
   const handleClick = () => {
@@ -18,13 +19,16 @@ const FileInput: React.FC<FileInputProps> = ({ setReport }) => {
     e.preventDefault()
     const reader = new FileReader()
     reader.onload = e => {
-      const parser = new XMLParser()
-      const xmlStr = e.target!.result
-      const jObj = parser.parse(xmlStr as string)
-      const report = createRunReport(jObj.RunReport)!
-      console.log(jObj)
-      console.log(report)
-      setReport(report)
+      try {
+        const parser = new XMLParser()
+        const xmlStr = e.target!.result
+        const jObj = parser.parse(xmlStr as string)
+        const report = createRunReport(jObj.RunReport)!
+        setError(false)
+        setReport(report)
+      } catch (err) {
+        setError(true)
+      }
     }
 
     if (e.target.files![0]) {
@@ -35,22 +39,19 @@ const FileInput: React.FC<FileInputProps> = ({ setReport }) => {
   return (
     <div
       onClick={handleClick}
-      className="transition ease-in-out delay-50 group mt-7 mb-6 border-md border-4 px-4 py-3 rounded-3xl text-sm font-bold font-mono cursor-pointer hover:bg-black"
+      className="transition ease-in-out delay-50 group mt-7 mb-4 border-md border-4 px-4 py-3 rounded-3xl text-sm font-bold font-mono cursor-pointer hover:bg-black"
     >
-      <label
-        htmlFor="upload-file"
-        className="transition ease-in-out delay-50 cursor-pointer group-hover:text-white"
-      >
+      <label className="transition ease-in-out delay-50 cursor-pointer group-hover:text-white">
         Choose Report
-        <input
-          type="file"
-          accept=".xml"
-          onChange={showFile}
-          id="upload-file"
-          ref={fileInput}
-          className="opacity-0 z-[-1] absolute"
-        />
       </label>
+      <input
+        type="file"
+        accept=".xml"
+        onChange={showFile}
+        id="upload-file"
+        ref={fileInput}
+        className="opacity-0 z-[-1] absolute"
+      />
     </div>
   )
 }
