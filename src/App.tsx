50ebb6c1eb952c8ecaa8ca485e74b870067d1114
John Bang <jeebeeng@gmail.com>
2022-03-04 13:46:51 -0500

    Change body bg-color to white

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [report, setReport] = useState<RunReport | null>(null)
 
   return (
-    <div className="bg-backBody min-h-screen flex flex-col items-center">
+    <div className="bg-white min-h-screen flex flex-col items-center">
       <h1 className="text-6xl font-rubik tracking-wide my-4">RISK REPORT</h1>
       <FileInput setReport={setReport} />
       {report != null ? <Report report={report!} /> : null}
