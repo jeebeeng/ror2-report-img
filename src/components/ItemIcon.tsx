a6a57be05d0feb737c2e227e06472fbb3664d7b3
John Bang <jeebeeng@gmail.com>
2022-03-04 20:18:37 -0500

    Adjust ItemIcon number style

--- src/components/ItemIcon.tsx
+++ src/components/ItemIcon.tsx
@@ -9,8 +9,8 @@ interface ItemIconProps {
 const ItemIcon: React.FC<ItemIconProps> = ({ item }) => {
   return (
     <div className="w-8 relative m-1 text-white">
-      <img src={path.itemImg(item.name)} alt={item.name} />
-      <p className="absolute top-0 right-0 bottom-2 text-xs">
+      <img className="mt-1" src={path.itemImg(item.name)} alt={item.name} />
+      <p className="absolute top-0 right-0 bottom-2 text-xs font-bold">
         {item.count > 1 ? `x${item.count}` : null}
       </p>
     </div>
