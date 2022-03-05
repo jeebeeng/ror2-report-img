1a0019e4cf4e37d48678be51dfd562e0eb2f2d15
John Bang <jeebeeng@gmail.com>
2022-03-04 21:59:53 -0500

    Make title banner into component

---
+++ src/components/TitleBanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TitleBanner = () => {
+  return (
+    <div className="grid place-items-center content-center bg-topBanner w-full text-center h-28">
+      <h1 className="text-7xl font-rubik tracking-wide text-white">
+        RISK REPORT
+      </h1>
+    </div>
+  )
+}
+
+export default TitleBanner
