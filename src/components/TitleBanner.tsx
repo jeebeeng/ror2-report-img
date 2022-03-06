3583c820bbb612b63409c7e02f741a0f9921e558
John Bang <jeebeeng@gmail.com>
2022-03-06 17:05:03 -0500

    Adjust title banner width

--- src/components/TitleBanner.tsx
+++ src/components/TitleBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TitleBanner = () => {
   return (
-    <div className="grid place-items-center content-center bg-topBanner w-full text-center h-28">
+    <div className="grid place-items-center content-center bg-topBanner w-screen text-center h-28">
       <h1 className="text-7xl font-rubik tracking-wide text-white">
         RISK REPORT
       </h1>
